160c5d88cd8241b1d1da693ece4a1a36606fd767
Sergio H. Valenzuela C <serginho61@gmail.com>
2023-03-31 12:15:24 -0400

    version listar.html dinamica

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('listar.html')
+    return render_template('listar.html', titulo='Canciones')
 
 app.run(host="0.0.0.0", port=5000)

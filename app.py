1ffc4beb4eaf46f41262ee0a8dccd5b97f00b59d
Sergio H. Valenzuela C <serginho61@gmail.com>
2023-04-14 09:56:39 -0400

    version 15-04-2023

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+class Cancion:
+    def __init__(self, titulo, categoria, idioma):
+        self.titulo = titulo
+        self.categoria = categoria
+        self.idioma = idioma
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('listar.html',titulo= 'Canciones')
-
+    cancion1= Cancion('La guitarra', 'Pop', 'Castellano')
+    cancion2= Cancion('Para no verte más', 'Pop', 'Castellano')
+    cancion3= Cancion('Balada para un gordo', 'Balada', 'Castellano')
+    lista=[cancion1, cancion2, cancion3]
+    return render_template('listar.html',titulo= 'canciones', musicas=lista)
 
 app.run(host="0.0.0.0", port=5000)

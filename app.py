dc149929616667ba549ba935dc66dab561c3a559
Sergio H. Valenzuela C <serginho61@gmail.com>
2023-05-18 22:13:46 -0400

    version 2023-05-18_03

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 # coding: utf-8
 from flask import Flask, render_template, request, url_for,redirect, session, flash
 
+class Usuario:
+    def __init__(self, nombre, nickname, clave):
+        self.nombre = nombre
+        self.nickname = nickname
+        self.clave = clave
+
+usuario1= Usuario('Jair Sampaio', 'JS', 'patitofeo')
+usuario2= Usuario('Rosa Flores', 'rosita', 'michifuz')
+usuario3= Usuario('Yami Moto Nokamina', 'kamikaze', 'sayonara')
+usuarios= { usuario1.nickname : usuario1,
+            usuario2.nickname : usuario2,
+            usuario3.nickname : usuario3
+          }
+
+
 class Cancion:
     def __init__(self, titulo, categoria, idioma):
         self.titulo = titulo
@@ -24,7 +39,7 @@ def index():
 def nuevoregistro():
     if session['usuario_logueado'] == None:
         flash('Usuario no conectado.')
-        return redirect('/login?proxima=nuevoregistro')
+        return redirect(url_for('login', proxima = url_for('nuevoregistro')))
     else:
         flash('Usuario conectado.') 
         return render_template('nuevoRegistro.html', titulo='Nueva Canción')
@@ -37,29 +52,36 @@ def crear():
     idioma = request.form['idioma']
     cancion = Cancion(titulo, categoria, idioma)
     lista.append(cancion)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
     return render_template('login.html', proxima = proxima)
 
+
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'patitofeo' == request.form['clave']:
-        session['usuario_logueado'] = request.form['usuario']
-        flash(session['usuario_logueado'] + ' ¡conectado con éxito!')
-        proxima_pagina = request.form['proxima']
-        return redirect('/{}'.format(proxima_pagina))
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['clave'] == usuario.clave:
+            session['usuario_logueado'] = usuario.nickname
+            flash(usuario.nickname + ' ¡conectado con éxito!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+        else:
+            flash('Usuario no conectado.')
+            return redirect(url_for('login', proxima = url_for('nuevoregistro')))
     else:
         flash('Usuario no conectado.')
-        return redirect('/login')
+        return redirect(url_for('login', proxima = url_for('nuevoregistro')))
 
+    
 @app.route('/logout')
 def logout():
     session['usuario_logueado'] = None
     flash('¡Logout efectuado exitosamente!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 app.run(host="0.0.0.0", port=5000)
